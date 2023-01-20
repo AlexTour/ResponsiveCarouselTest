aae47d1e713da4b881f867c26f5e30f39ff884ea
Alexandros Touramanis <32987136+AlexTour@users.noreply.github.com>
2023-01-20 11:28:59 +0100

    Create dataLoader.js

---
+++ Scripts/dataLoader.js
@@ -0,0 +1,21 @@
+document.getElementById('logo').src = "Assets/dali-logo.png";
+
+document.getElementById('room01-title').textContent = "ERDGESCHOSS (1/2)";
+document.getElementById('room01-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image01-container").style.backgroundImage = "url('./Assets/poster01.png')"; 
+
+document.getElementById('room02-title').textContent = "ERDGESCHOSS (2/2)";
+document.getElementById('room02-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image02-container").style.backgroundImage = "url('./Assets/poster02.png')"; 
+
+document.getElementById('room03-title').textContent = "OBERGESCHOSS (1/3)";
+document.getElementById('room03-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image03-container").style.backgroundImage = "url('./Assets/poster03.png')"; 
+
+document.getElementById('room04-title').textContent = "OBERGESCHOSS (2/3)";
+document.getElementById('room04-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image04-container").style.backgroundImage = "url('./Assets/poster04.png')"; 
+
+document.getElementById('room05-title').textContent = "OBERGESCHOSS (3/3)";
+document.getElementById('room05-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image05-container").style.backgroundImage = "url('./Assets/poster05.png')"; 
