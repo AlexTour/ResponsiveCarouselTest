a204c32d55991ff51cbc8dd8c040c2a74a35abea
Alexandros Touramanis <32987136+AlexTour@users.noreply.github.com>
2023-02-20 16:04:26 +0200

    Update dataLoader.js

--- Scripts/dataLoader.js
+++ Scripts/dataLoader.js
@@ -22,4 +22,40 @@ document.getElementById("image05-container").src = "./Assets/poster05.png";
 
 document.getElementById('room06-title').textContent = "ROOM 6";
 document.getElementById('room06-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
-document.getElementById("image06-container").src = "./Assets/poster06.png"; 
+document.getElementById("image06-container").src = "./Assets/poster06.png";
+
+document.getElementById('room07-title').textContent = "ROOM 7";
+document.getElementById('room07-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image07-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room08-title').textContent = "ROOM 8";
+document.getElementById('room08-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image08-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room09-title').textContent = "ROOM 9";
+document.getElementById('room09-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image09-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room10-title').textContent = "ROOM 10";
+document.getElementById('room10-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image10-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room11-title').textContent = "ROOM 11";
+document.getElementById('room11-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image11-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room12-title').textContent = "ROOM 12";
+document.getElementById('room12-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image12-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room13-title').textContent = "ROOM 13";
+document.getElementById('room13-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image13-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room14-title').textContent = "ROOM 14";
+document.getElementById('room14-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image14-container").src = "./Assets/poster06.png"; 
+
+document.getElementById('room15-title').textContent = "ROOM 15";
+document.getElementById('room15-description').textContent = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis nat";
+document.getElementById("image15-container").src = "./Assets/poster06.png"; 
